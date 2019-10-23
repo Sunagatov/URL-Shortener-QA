f8492a494f977fcc2bc565268c433171dbc12c39
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-23 22:45:13 +0400

    Ready to use

---
+++ src/main/java/com/zufar/registry/Application.java
@@ -0,0 +1,13 @@
+package com.zufar.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryServerApplication.class, args);
+    }
+}
