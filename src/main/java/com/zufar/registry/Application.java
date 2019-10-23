5224e661bab58531bea3a60debb626d6c0bceb92
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-23 22:55:06 +0400

    RegistryServerApplication was renamed to Application. Pom.xml and application.yml were fixed.

--- src/main/java/com/zufar/registry/Application.java
+++ src/main/java/com/zufar/registry/Application.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class RegistryServerApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(RegistryServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
