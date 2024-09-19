776e355faf3f001b55faf974c70d2a59baa74b1f
TetianaPerinha <68566253+TetianaPerinha@users.noreply.github.com>
2024-09-19 11:46:17 -0500

    new:test_recreate_short_url,conftest.py modify:api_short_link.py

--- API/TEST/test_create_short_url.py
+++ API/TEST/test_create_short_url.py
@@ -1,6 +1,6 @@
 import allure
 from allure import step
-from hamcrest import assert_that, is_
+from hamcrest import assert_that, is_, is_not
 
 from API.FRAMEWORK.api_endpoints.api_short_link import ShorteningLinkAPI
 from API.FRAMEWORK.assertion.assert_content_type import assert_content_type
@@ -30,8 +30,9 @@ class TestCreateShortUrl:
             assert_content_type(response, "application/json")
 
         with step("Verify short url"):
-            assert response.json()["shortUrl"] @ allure.severity(allure.severity_level.CRITICAL)
+            assert response.json()["shortUrl"], is_not(None)
 
+    @allure.severity(allure.severity_level.CRITICAL)
     @allure.title("Test recreation short url, happy path")
     @allure.description(
         """WHEN user send POST request to create short url for the URL already shortened before   
@@ -50,4 +51,3 @@ class TestCreateShortUrl:
                 "Verify that created short url after second POST request for origin URL is the same as was created before"):
             assert_that(recreated_short_url, is_(short_url),
                         reason="Recreated short url is not the same as was created before")
-
