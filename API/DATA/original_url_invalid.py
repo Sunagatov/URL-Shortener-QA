1e722888e15b838d444306fd6f6164ecf07232fc
ilsinyakov <ilsinyakov@gmail.com>
2024-09-21 19:45:35 +0300

    test create short url negative

---
+++ API/DATA/original_url_invalid.py
@@ -0,0 +1,14 @@
+ORIGINAL_URL_INVALID = (
+    ('http://example.com/search?query=a%40b%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2Fy%2Fz%2Fa%2Fb%2Fc%2Fd%2Fe%2Ff%2Fg%2Fh%2Fi%2Fj%2Fk%2Fl%2Fm%2Fn%2Fo%2Fp%2Fq%2Fr%2Fs%2Ft%2Fu%2Fv%2Fw%2Fx%2',
+     'URL is too long'),
+    ('',
+     'URL must not be empty or blank'),
+    (' ',
+     'URL must not be empty or blank'),
+    ('https://ic. ed-la. tte.uk/',
+     'URL must not contain spaces'),
+    ('http://.com',
+     'URL must have a valid host'),
+    ('ftp://example.com/file',
+     'URL must use HTTP or HTTPS protocol')
+)
---
+++ API/TEST/test_create_short_url_negative.py
@@ -0,0 +1,34 @@
+import allure
+from allure import step
+from hamcrest import is_
+import pytest
+
+from API.DATA.original_url_invalid import ORIGINAL_URL_INVALID
+from API.FRAMEWORK.api_endpoints.api_short_link import ShorteningLinkAPI
+from API.FRAMEWORK.assertion.assert_content_type import assert_content_type
+from API.FRAMEWORK.assertion.assert_status_code import assert_status_code
+
+
+@allure.feature("Short URL generation")
+@allure.link("https://short-link.zufargroup.com/webjars/swagger-ui/index.html#/URL%20Shortening/shortenUrl",
+             name="Swagger")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.title("Negative test create short url")
+@allure.description(
+    """WHEN user send POST request to create short url
+       AND original URL is not valid
+       THEN status HTTP CODE = 400 and response body contains corresponding error message"""
+)
+@pytest.mark.parametrize('original_url', 'error_message', ORIGINAL_URL_INVALID)
+def test_create_short_url_negative(original_url, error_message):
+    with step("Send POST request to create short url"):
+        response = ShorteningLinkAPI().shorten_link(original_url)
+
+    with step("Verify status code"):
+        assert_status_code(response, 400)
+
+    with step("Verify content-type"):
+        assert_content_type(response, "application/json")
+
+    with step("Verify error message"):
+        assert response.json()["errorMessage"], is_(error_message)
