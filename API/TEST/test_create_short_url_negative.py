5c9b933e48f546ab9f91698ee19281562f59b395
ilsinyakov <ilsinyakov@gmail.com>
2024-09-22 10:44:45 +0300

    test create short url negative 2

--- API/TEST/test_create_short_url_negative.py
+++ API/TEST/test_create_short_url_negative.py
@@ -19,7 +19,7 @@ from API.FRAMEWORK.assertion.assert_status_code import assert_status_code
        AND original URL is not valid
        THEN status HTTP CODE = 400 and response body contains corresponding error message"""
 )
-@pytest.mark.parametrize('original_url', 'error_message', ORIGINAL_URL_INVALID)
+@pytest.mark.parametrize('original_url, error_message', ORIGINAL_URL_INVALID)
 def test_create_short_url_negative(original_url, error_message):
     with step("Send POST request to create short url"):
         response = ShorteningLinkAPI().shorten_link(original_url)
